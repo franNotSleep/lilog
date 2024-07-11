220e6ce3116ef99b964afe020b604b610b0419c1
francisco jose <frano04930@gmail.com>
2024-07-10 20:19:24 -0400

    memkvs

---
+++ lilog/internal/adapters/db/memkvs.go
@@ -0,0 +1,28 @@
+package db
+
+import "github.com/frannotsleep/lilog/internal/application/core/domain"
+
+type MemKVSAdapter struct {
+	kvs map[int32][]domain.Invoice
+}
+
+func NewMemKVSAdapter() *MemKVSAdapter {
+	return &MemKVSAdapter{
+		kvs: make(map[int32][]domain.Invoice),
+	}
+}
+
+func (m *MemKVSAdapter) Save(invoice domain.Invoice) error {
+	if len(m.kvs[invoice.PID]) < 0 {
+		m.kvs[invoice.PID] = append(m.kvs[invoice.PID], invoice)
+	} else {
+		m.kvs[invoice.PID] = []domain.Invoice{invoice}
+	}
+
+	return nil
+}
+
+func (m *MemKVSAdapter) Get(pid int32) ([]domain.Invoice, error){
+  invoices :=  m.kvs[pid]
+  return invoices, nil
+}
