61bf85bebcbd07f7cb3892be51100f0a89c1415f
francisco jose <frano04930@gmail.com>
2024-07-20 23:31:42 -0400

    using context to stop server

--- lilog/cmd/main.go
+++ lilog/cmd/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"github.com/frannotsleep/lilog/internal/adapters/cmdServer"
 	"github.com/frannotsleep/lilog/internal/adapters/db"
+	"github.com/frannotsleep/lilog/internal/adapters/repl"
+	"github.com/frannotsleep/lilog/internal/adapters/server"
 	"github.com/frannotsleep/lilog/internal/application/core/api"
 )
 
 func main() {
 	memDBAdapter := db.NewMemKVSAdapter()
-  app := api.NewApplication(memDBAdapter)
-	server := cmdServer.NewAdapter(memDBAdapter, "127.0.0.1:4119", app)
+	api := api.NewApplication(memDBAdapter)
 
-	server.Run()
+  replAdapter, ctx := repl.NewAdapter(api)
+
+	connConfig := server.ConnConfig{Address: "127.0.0.1:4119", AllowedClients: []string{"127.0.0.1:5697"}}
+	serverAdapter := server.NewAdapter(api, connConfig, ctx)
+
+	//	server := cmdServer.NewAdapter("127.0.0.1:4119", api)
+
+	// server.Run()
+	go serverAdapter.ListenAndServe()
+  replAdapter.Run()
 }
