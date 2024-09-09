1d83de97da69be279d5727278d55f2fd38f9d407
francisco jose <frano04930@gmail.com>
2024-09-08 21:15:19 -0400

    bug fix with get

--- lilog/internal/adapters/db/sqliteDB.go
+++ lilog/internal/adapters/db/sqliteDB.go
@@ -102,7 +102,7 @@ func (m *SqliteAdapter) Get(server string) ([]domain.Invoice, error) {
 		if err := rows.Scan(&invoice.ID, &invoice.Server, &invoice.Time, &invoice.Level, &invoice.Hostname, &invoice.ResponseTime, &invoice.Message, &invoice.InvoiceResponse.StatusCode, &invoice.InvoiceRequest.Method, &invoice.InvoiceRequest.URL, &invoice.InvoiceRequest.RemoteAddress, &invoice.InvoiceRequest.RemotePort); err != nil {
 			return invoices, err
 		}
-		invoices = append(invoices, invoices...)
+		invoices = append(invoices, invoice)
 	}
 
 	if err = rows.Err(); err != nil {
@@ -117,5 +117,30 @@ func (m *SqliteAdapter) Servers() ([]string, error) {
 }
 
 func (m *SqliteAdapter) Export() ([]domain.Invoice, error) {
-	return []domain.Invoice{}, nil
+
+	rows, err := m.db.Query("SELECT * FROM " + INVOICE_TABLE + ";")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	invoices := []domain.Invoice{}
+
+	for rows.Next() {
+		invoice := domain.Invoice{
+			InvoiceRequest:  domain.InvoiceRequest{},
+			InvoiceResponse: domain.InvoiceResponse{},
+		}
+
+		if err := rows.Scan(&invoice.ID, &invoice.Server, &invoice.Time, &invoice.Level, &invoice.Hostname, &invoice.ResponseTime, &invoice.Message, &invoice.InvoiceResponse.StatusCode, &invoice.InvoiceRequest.Method, &invoice.InvoiceRequest.URL, &invoice.InvoiceRequest.RemoteAddress, &invoice.InvoiceRequest.RemotePort); err != nil {
+			return invoices, err
+		}
+		invoices = append(invoices, invoices...)
+	}
+
+	if err = rows.Err(); err != nil {
+		return invoices, err
+	}
+
+	return invoices, nil
 }
